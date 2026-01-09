0c17eb8f63a8cad6b91fb163bcfcdaa7ab55c6cf
Erick <erick.galdino@gfxconsultoria.com>
2026-01-09 13:12:42 -0300

    Css do login finalizado

--- web/frontend/src/pages/login.jsx
+++ web/frontend/src/pages/login.jsx
@@ -2,6 +2,8 @@
 import { useEffect, useRef } from 'react'
 //import { useNavigate } from 'react-router-dom';
 import '../assets/css/global.css'
+import './css/login.css'
+import logo from '../assets/logo.svg'
 
 
 
@@ -16,13 +18,15 @@ function Login() {
     return (
         <div className="login-container">
             <form action="">
-                <img src="#" alt="Logo do site" />
-                <div>
-                    <input type="text" placeholder="Username" ref={inputUsername} />
-                    <input type="password" placeholder="Password" ref={inputPassword} />
+                <img src={logo} alt="Logo do site" />
+                <div className="input-group-login">
+                    <input type="text" placeholder="Usuario" ref={inputUsername} />
+                    <input type="password" placeholder="Senha" ref={inputPassword} />
                 </div>
-                <button>LOGIN</button>
-                <button>Esqueci a senha</button>
+                <div className='button-group-login'>
+                    <button className="login-button">LOGIN</button>
+                    <button className="forgot-password-button">Esqueci a senha</button>
+                </div>
             </form>
         </div>
     );
